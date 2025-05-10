fdc874af97ed5e61db67a66332303cce5ae435cd
yeseul <yesul0718@naver.com>
2025-05-10 20:03:19 +0900

    Apply DB and  untrack selected files

--- deploy-ai-platform.sh
+++ deploy-ai-platform.sh
@@ -6,9 +6,11 @@ set -e  # 에러 시 종료
 echo "Pulling Docker images..."
 docker pull yeseul01/ocr-api:latest
 docker pull yeseul01/inference-service:latest
+docker pull yeseul01/postgres-custom:latest
 
 # 2. Kubernetes 리소스 배포
 echo "Applying Kubernetes resources..."
+kubectl apply -f postgres-deploy.yaml
 kubectl apply -f ocr-deploy.yaml
 kubectl apply -f inference-deploy.yaml
 kubectl apply -f ingress.yaml
@@ -18,3 +20,6 @@ echo "서비스 경로:"
 echo " - /ocr → ocr-api (port 9000)"
 echo " - /predict → inference-service (port 8080)"
 echo " - /      → inference-service (port 8080)"
+
+kubectl get pods
+kubectl get ingress
